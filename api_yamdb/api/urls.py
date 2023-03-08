baa02026e24d178dfff239d637cb8e891aeefb73
Арсен Григорян <arsen8577711@yandex.ru>
2023-03-08 10:33:00 +0300

    Отправляю на ревью

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,8 @@
-from django.urls import include, path
-from rest_framework import routers
 from .views import (CommentsViewSet, CategoriesViewSet, GenresViewSet,
                     ReviewViewSet, SignUpViewSet, TitleViewSet, TokenViewSet,
                     UsersViewSet)
+from django.urls import include, path
+from rest_framework import routers
 
 router_v1 = routers.DefaultRouter()
 router_v1.register(r'titles', TitleViewSet, basename='titles')
